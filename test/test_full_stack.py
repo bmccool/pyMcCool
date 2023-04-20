5499b18e51d666877805aae6638b967411656b7d
Brendon McCool <brendonmccool@gmail.com>
2023-04-20 18:13:56 -0400

    Move isolate e2e functions

--- test/test_full_stack.py
+++ test/test_full_stack.py
@@ -1,5 +1,6 @@
 """ Tests for logging/tracing combined """
 from contextlib import redirect_stdout
+from collections import namedtuple
 import functools
 import io
 from uuid import uuid1
@@ -10,84 +11,38 @@ import pytest
 from pymccool.logging import Logger, LoggerKwargs
 from pymccool.tracing import get_tracer, get_decorator
 
-def mock_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-    """ Mock Decorator """
-    @functools.wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        return func(*args, **kwargs)
-    return wrapper
-
-def get_mock_decorator():
-    """ Return mock tracing decorator as paceholder """
-    return mock_decorator
-
-
-uuid = uuid1()
-string_capture = io.StringIO()
-with redirect_stdout(string_capture):
-    logger = Logger(
-        LoggerKwargs(
-            app_name="test_logger_loki",
-            default_level=Logger.VERBOSE,
-            stream_level=Logger.VERBOSE,
-            grafana_loki_endpoint="https://loki.capricorn.brendonmccool.com/loki/api/v1/push",
-            uuid=uuid)
-    )
-
-instrument = mock_decorator
 
 @pytest.fixture(autouse=True, scope="module")
-def session_fixture():
+def session_fixture(e2e_instrument):
     """
     Provide a fixture to handle setup/teardown for the module
+    Note 1: Because the instrument decorator relies on external services 
+    which may not be availble, we decorate "manually" inside this session
+    fixture, which garuntees the tests have been marked for execution.
     """
-    global instrument
-    global uuid
-    tracer = get_tracer(service_name="test_tracer",
-                        endpoint="https://otel-rec.capricorn.brendonmccool.com/v1/traces",
-                        uuid=uuid)
-    instrument = get_decorator(tracer)
+    global set_test_point
+    global verify_properties
+    instrument = e2e_instrument
+    set_test_point = instrument(set_test_point)
+    verify_properties = instrument(verify_properties)
     yield
-    logger.close()
 
 
-
-@instrument
-def func_a(*args, **kwargs):
-    func_b()
-    func_b()
-
-@instrument
-def func_b(*args, **kwargs):
-    func_c()
-    func_c()
-    func_d()
-    func_c()
-
-@instrument
-def func_c(*args, **kwargs):
-    time.sleep(1)
-
-@instrument
-def func_d(*args, **kwargs):
-    func_c()
-    func_c()
-
-@instrument
-def set_test_point(temperature: int, voltage: int) -> None:
+#@instrument See Note 1
+def set_test_point(temperature: int, voltage: int, logger: Logger) -> None:
     logger.info(f"Setting test point to {temperature}C, {voltage}mV")
     while not bool(random.getrandbits(1)):
         logger.debug("Still waiting for DUT to report correct test point...")
     logger.info("Test point set!")
     
-@instrument
-def verify_properties(temperature: int, voltage: int) -> None:
+#@instrument See Note 1
+def verify_properties(temperature: int, voltage: int, logger: Logger) -> None:
     logger.info("Verifying region properties")
-    set_test_point(temperature=temperature, voltage=voltage)
+    set_test_point(temperature=temperature, voltage=voltage, logger=logger)
 
 @pytest.mark.e2e
-def test_region_properties():
+def test_region_properties(e2e_logger):
     """
     Demo for full stack test
     """
-    verify_properties(30, 4000)
+    verify_properties(temperature=30, voltage=4000, logger=e2e_logger)
--- test/test_tracer.py
+++ test/test_tracer.py
@@ -4,45 +4,31 @@ import time
 import mock
 from uuid import uuid1
 import pytest
+from opentelemetry.trace import Tracer
 from pymccool.tracing import get_tracer, get_decorator
 
-
-
-uuid = uuid1()
-tracer = get_tracer(service_name="test_tracer",
-                    endpoint="https://otel-rec.capricorn.brendonmccool.com/v1/traces",
-                    uuid=uuid,
-                    otlp=False)
-instrument = get_decorator(tracer)
-
-@instrument
 def func_a(*args, **kwargs):
     func_b()
     func_b()
 
-@instrument
 def func_b(*args, **kwargs):
     func_c()
     func_c()
     func_d()
     func_c()
 
-@instrument
 def func_c(*args, **kwargs):
     time.sleep(0.01)
 
-@instrument
 def func_d(*args, **kwargs):
     func_c()
     func_c()
-
 
 @pytest.mark.e2e
 def test_tracer():
     """
     Basic tracer test
     """
-
     func_a()
 
 def test_tracer_unit():
@@ -55,23 +41,18 @@ def test_tracer_unit():
 
 
 def datetime2ns(dtime: datetime) -> int:
-    #nanos = time.replace(tzinfo=timezone.utc).timestamp()*10**9
     nanos = dtime.timestamp()*10**9
     return int(nanos)
 
 @pytest.mark.e2e
-def test_tracer_custom_time():
-    
+def test_tracer_custom_time(e2e_tracer):
+    tracer: Tracer = e2e_tracer
     start_time = datetime.now()# - timedelta(hours=12)
-    #start_time = time.time_ns() - (1000 * 1000 * 1000 * 60 * 30)
     with tracer.start_as_current_span(name="CustomTimeSpan", start_time=datetime2ns(start_time), end_on_exit=False) as span:
-
-        
         time.sleep(1)
         span.end(end_time=datetime2ns(start_time + timedelta(minutes=30)))
 
 
-@instrument
 def stackable_function(recursions: int, sleep_time_s: float):
     time.sleep(sleep_time_s)
     if recursions > 0:
@@ -82,3 +63,23 @@ def stackable_function(recursions: int, sleep_time_s: float):
 def test_tracer_stacking():
     stackable_function(5, 0.01)
 
+@pytest.fixture(autouse=True, scope="module")
+def tracer_session_fixture(e2e_instrument):
+    """
+    Provide a fixture to handle setup/teardown for the module
+    Note 1: Because the instrument decorator relies on external services 
+    which may not be availble, we decorate "manually" inside this session
+    fixture, which garuntees the tests have been marked for execution.
+    """
+    global func_a
+    global func_b
+    global func_c
+    global func_d
+    global stackable_function
+    instrument = e2e_instrument
+    func_a = instrument(func_a)
+    func_b = instrument(func_b)
+    func_c = instrument(func_c)
+    func_d = instrument(func_d)
+    stackable_function = instrument(stackable_function)
+    yield
